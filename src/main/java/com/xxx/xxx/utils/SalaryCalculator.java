fe342046e0f39e33af4f5f4e65288ef4d0a9f889
codefresher <codefresher@gmail.com>
2019-07-25 01:26:12 +0800

    update file from wen hao

--- src/main/java/com/xxx/xxx/utils/SalaryCalculator.java
+++ src/main/java/com/xxx/xxx/utils/SalaryCalculator.java
@@ -27,11 +27,15 @@ import java.math.BigDecimal;
 public class SalaryCalculator {
 
     static {
-        TimeUnit.sleep(5000);
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static BigDecimal calculate(BigDecimal salary) {
-        TimeUnit.sleep(5000);
+        TimeUnit.sleep(7000);
         return salary.add(BigDecimal.TEN);
     }
 }
